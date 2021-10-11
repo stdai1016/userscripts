0cc8d5ba3f384829d9bfa334c3fb6561d6cd63cc
stdai <stdai1016@gmail.com>
2021-10-12 00:39:43 +0800

    extends Response

--- GM_fetch.user.js
+++ GM_fetch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GM_fetch
 // @description  GM_fetch
-// @version      0.1.0a
+// @version      0.1.0b
 // @license      MIT
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
@@ -9,14 +9,30 @@
 /**
  *  @param {RequestInfo} resource
  *  @param {RequestInit} init
- *  @returns {Promise<Response>}
+ *  @returns {Promise<GM_Response>} extension of `Response`
  */
+
 async function GM_fetch (input, init = null) { // eslint-disable-line
+  class GM_Response extends Response { // eslint-disable-line camelcase
+    constructor (body = null, init = null) {
+      super(body, init);
+      this._url = init && init.url ? init.url : super.url;
+      this._redirected =
+        init && init.redirected ? init.redirected : super.redirected;
+      this._type = init && init.type ? init.type : super.type;
+    }
+
+    get url () { return this._url; }
+    get redirected () { return this._redirected; }
+    get type () { return this._type; }
+  }
+
   function cvtHeaders (headers) {
     const h = {};
     for (const pair of headers.entries()) h[pair[0]] = pair[1];
     return h;
   }
+
   function parseHeaders (strHeaders) {
     const h = new Headers();
     strHeaders.trim().split('\r\n').forEach(line => {
@@ -40,13 +56,13 @@ async function GM_fetch (input, init = null) { // eslint-disable-line
       onerror: () => reject(new TypeError('fetch error')),
       ontimeout: () => reject(new TypeError('fetch timeout')),
       onload: function (resp) {
-        const response = new Response(resp.response, {
+        const response = new GM_Response(resp.response, {
           status: resp.status,
           statusText: resp.statusText,
           headers: parseHeaders(resp.responseHeaders),
-          url: resp.finalUrl
+          url: resp.finalUrl,
+          redirected: resp.finalUrl !== request.url.split('#')[0]
         });
-        response.url = resp.finalUrl;
         resolve(response);
       }
     });
