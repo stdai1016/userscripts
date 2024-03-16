3738a8704a1add38380511628732f4f64c9f218c
stdai <stdai1016@gmail.com>
2024-03-17 03:58:21 +0800

    add userscript `multiFileDownload`

---
+++ utils/multiFileDownload.user.js
@@ -0,0 +1,96 @@
+// ==UserScript==
+// @name         MultiFileDownload
+// @description  Multi thread downloader
+// @version      0.1.0
+// @license      MIT
+// @namespace    https://github.com/stdai1016
+// ==/UserScript==
+
+/**
+ * @callback ProgressEventListener
+ * @param {ProgressEvent} event
+ */
+
+/**
+ * @callback FetchFunction
+ * @param {RequestInfo|URL} input
+ * @param {RequestInit=} init
+ * @returns {Promise<Response>}
+ */
+
+/**
+ * @param {{href: string, download?: string}[]} anchors
+ * @param {Object} options
+ * @param {int=} options.retry
+ * @param {int=} options.threads
+ * @param {ProgressEventListener=} options.onprogress
+ * @param {FetchFunction=} options.fetch
+ * @returns {Promise<File[]>}
+ */
+async function multiFileDownload (anchors, options = {}) { // eslint-disable-line
+  if (typeof anchors[Symbol.iterator] !== 'function') {
+    anchors = [anchors];
+  }
+  anchors = [...anchors];
+
+  const files = [];
+  const total = anchors.length;
+  const retryCounter = {};
+  const retry = options.retry ?? 5;
+  const threads = Math.max(options.threads ?? 1, 1);
+  const fetchFunction = options.fetch ?? fetch;
+  const onprogress = (type) => {
+    if (options.onprogress) {
+      setTimeout(() => {
+        options.onprogress(new ProgressEvent(type, {
+          lengthComputable: true,
+          loaded: files.length,
+          total: total
+        }));
+      }, 0);
+    }
+  };
+
+  onprogress('loadstart');
+
+  const downloaders = [...Array(threads).keys()].map((id) => {
+    const downloadQueue = async function () {
+      if (!anchors.length) return;
+      const a = anchors.shift();
+      const url = new URL(a.href);
+      let name = a.download?.toString() ?? '';
+      name = name.length ? name : url.pathname.split(/[\\/]/).pop();
+      if (!name.match(/(?:\.([^.]+))?$/)) {
+        name += url.pathname.match(/(?:\.([^.]+))?$/)[1] ?? '';
+      }
+      console.debug(`Downloader#${id}: download "${name}" (${url})`);
+
+      const resp = await fetchFunction(url, {
+        headers: { 'cache-control': 'no-cache', referer: location.origin }
+      });
+      const blob = resp.status === 200 ? await resp.blob() : new Blob();
+      if (blob.size) {
+        files.push(new File([blob], name, { type: blob.type }));
+        onprogress('progress');
+      } else {
+        retryCounter[url] = (retryCounter[url] ?? retry) - 1;
+        if (retryCounter[url] === 0) {
+          throw new Error(`Download failed: ${name} (${url}`);
+        }
+        anchors.push(a);
+      }
+      return downloadQueue();
+    };
+    return downloadQueue();
+  });
+
+  return Promise.all(downloaders).then(() => {
+    onprogress('load');
+    onprogress('loadend');
+    return files;
+  }).catch((e) => {
+    onprogress('error');
+    onprogress('loadend');
+    throw e;
+  });
+}
