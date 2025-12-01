67174c7449ff433f44d70b05708cf54c563e580c
stdai <stdai1016@gmail.com>
2025-12-01 21:12:16 +0800

    update orderhistory

--- asobistore/orderhistory.user.js
+++ asobistore/orderhistory.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Orderhistory Helper
 // @description  Help to navigate to product pages from order history
-// @version      2025-11-28
+// @version      2025-12-01
 // @license      MIT
 // @homepage     https://github.com/stdai1016
 // @namespace    https://github.com/stdai1016/userscripts/asobistore
@@ -13,19 +13,34 @@
 (function () {
   'use strict';
 
-  const idCells = document.querySelectorAll('table.cart_table td.td04');
-  [...idCells].forEach((cell) => {
+  const navigateToProductPage = (text) => {
+    const href = `https://shop.asobistore.jp/products/detail/${text}`;
+    console.debug(`Navigating to: ${href}`);
+    const anchor = document.createElement('a');
+    anchor.href = href;
+    anchor.target = '_blank';
+    anchor.rel = 'noopener noreferrer';
+    anchor.click();
+  };
+  const copyToClipboard = (text) => {
+    console.debug(`Copied to clipboard: ${text}`);
+    navigator.clipboard.writeText(text);
+  };
+
+  const cells = document.querySelectorAll('table.cart_table td');
+  [...cells].forEach((cell) => {
     cell.addEventListener('click', (e) => {
       e.preventDefault();
       e.stopPropagation();
-      const text = cell.innerText.trim();
-      const href = `https://shop.asobistore.jp/products/detail/${text}`;
-      console.debug(`Navigating to: ${href}`);
-      const anchor = document.createElement('a');
-      anchor.href = href;
-      anchor.target = '_blank';
-      anchor.rel = 'noopener noreferrer';
-      anchor.click();
+      if (cell.classList.contains('td04')) {
+        if (cell.hasAttribute('nowrap')) {
+          navigateToProductPage(cell.innerText.trim());
+        } else {
+          copyToClipboard(cell.innerText.trim());
+        }
+      } else if (cell.classList.contains('td03')) {
+        copyToClipboard(cell.innerText.replace(/,/g, '').replace(/[^\d]/g, ''));
+      }
     });
   });
 })();
