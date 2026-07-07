544eab8d0b63e34cbf722514cd15c616f1e3c396
stdai <stdai1016@gmail.com>
2026-07-08 01:31:06 +0800

    fix error

--- x_com/twimg_orig_redirect.user.js
+++ x_com/twimg_orig_redirect.user.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name        twimg:orig Redirect
 // @description Go to orignal size URL of Twitter image
-// @version     0.3.4
+// @version     0.3.5
 // @license     MIT
 // @homepage    https://github.com/stdai0a10
 // @namespace   https://github.com/stdai0a10/userscripts/x_com
@@ -19,10 +19,11 @@
 (function () {
   const fmtLegacy = window.localStorage.getItem('fmtLegacy');
   const f = fmtLegacy ? ['.', ':'] : ['?format=', '&name='];
+  const matcher =
+    /^(https?:\/\/pbs\.twimg\.com\/media\/[-\w]+)(?:\?(?:[\w=]+&)*format=|\.)(jpg|png)/;
+
   const href = window.location.href;
-  const m = href.match(
-    /^(https?:\/\/pbs\.twimg\.com\/media\/\w+)(?:\?(?:[\w=]+&)*format=|\.)(jpg|png)/
-  );
+  const m = href.match(matcher);
   const url = m[1] + f[0] + m[2] + f[1] + 'orig';
 
   if (window.sessionStorage.getItem('url') !== url && url !== href) {
